d381adc48a39d87531fc04725a7d1b03c81c71c3
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2015-11-15 19:45:11 +0100

    Pipeline refactored

--- blob-detect/src/main/scala/ProcessingStep.scala
+++ blob-detect/src/main/scala/ProcessingStep.scala
@@ -13,38 +13,47 @@ object ProcessingStep{
 
   case class ProcessingStepInfo[T]( mat : Mat, info : T )
 
+  type Process[SRC,DST] = (ProcessingStepInfo[SRC]) => ProcessingStepInfo[DST]
+
   trait ProcessingStep[SRC,DST] {
-    val process: (ProcessingStepInfo[SRC]) => ProcessingStepInfo[DST]
+    val process: Process[SRC,DST]
     val stepName: String
+
+    def extend[T](name: String)(p: Process[DST,T]) = ExtendedStep(this,name,p)
   }
 
-  case class Step[S,D](override val stepName: String, override val process: (ProcessingStepInfo[S]) => ProcessingStepInfo[D]) extends ProcessingStep[S,D]
+  case class ExtendedStep[SRC,DST,T]( previous: ProcessingStep[SRC,DST], override val stepName: String, extendedProcess: Process[DST,T] ) extends ProcessingStep[SRC,T]{
+    override val process = (psi:ProcessingStepInfo[SRC]) => extendedProcess( previous.process(psi) )
+  }
 
-  def withDrawContours[T]( s: Step[T,Seq[MatOfPoint]]) = {
-    def drawContoursProcess( psi: ProcessingStepInfo[Seq[MatOfPoint]] ) = {
+  case class InitialStep(override val stepName: String) extends ProcessingStep[Unit,Unit]{
+    override val process = matrixOnlyProcess(m=>m)
+  }
+
+  def withDrawContours[T]( s: ProcessingStep[T,Seq[MatOfPoint]]) = {
+    s.extend( s.stepName){ psi =>
       val ret = psi.mat.clone()
       drawContours( ret, psi.info, new Scalar(255, 0, 255), 3 )
       ProcessingStepInfo( ret, psi.info )
     }
-
-    Step( s.stepName, s.process andThen drawContoursProcess )
   }
 
-
-  def matrixOnlyProcess( proc: (Mat)=>Mat ) = {
+  implicit def matrixOnlyProcess( proc: (Mat)=>Mat ): (ProcessingStepInfo[Unit]) => ProcessingStepInfo[Unit] = {
     ( psi : ProcessingStepInfo[Unit] ) => ProcessingStepInfo( proc(psi.mat), Unit )
   }
 
-  val steps = Seq(
-    Step("Video original",  matrixOnlyProcess(m => m) ),
-    Step("Umbral adaptativo", matrixOnlyProcess( threshold() )),
-    Step("Eliminación de ruido (open-close)", matrixOnlyProcess( clean()()) ),
-    withDrawContours( Step("Búsqueda de contornos", (psi : ProcessingStepInfo[Unit]) => ProcessingStepInfo( psi.mat, findContours(psi.mat) ) ) )
-      /*
-    Step("Filtro de contronos no cuadriláteros", detectContours(approximateContoursToQuadrilaterals()) ),
-    Step("Los mayores cinco cuadriláteros", detectContours( approximateContoursToQuadrilaterals() _ andThen findBiggestAlignedQuadrilaterals() ) ),
-    Step("Tabla de respuestas", detectContours( approximateContoursToQuadrilaterals() _ andThen findBiggestAlignedQuadrilaterals() andThen locateAnswerMatrixAsSeq ) )
-    */
+  implicit def contoursOnlyProcess( proc: Seq[MatOfPoint] => Seq[MatOfPoint] ) : (ProcessingStepInfo[Seq[MatOfPoint]]) => ProcessingStepInfo[Seq[MatOfPoint]] = {
+    ( psi : ProcessingStepInfo[Seq[MatOfPoint]] ) => ProcessingStepInfo( psi.mat, proc(psi.info) )
+  }
+
+  val steps : Pair[String,Process[_,_]] = Seq(
+    ("Umbral adaptativo", threshold() _ ),
+    ("Eliminación de ruido (open-close)", clean()() _ ),
+    ("Búsqueda de contornos", (psi : ProcessingStepInfo[Unit]) => ProcessingStepInfo( psi.mat, findContours(psi.mat) ) ),
+    ("Filtro de contronos no cuadriláteros", approximateContoursToQuadrilaterals() _ ),
+    ("Los mayores cinco cuadriláteros", findBiggestAlignedQuadrilaterals()  _ ),
+    ("Tabla de respuestas", (psi: ProcessingStepInfo[Seq[MatOfPoint]] ) => ProcessingStepInfo( psi.mat, locateAnswerMatrix(psi.info) ) )
+
   )
 
 }
