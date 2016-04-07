8f08ed15db4003ada0897ceb8131179344475a5e
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2016-04-07 18:06:42 +0200

    Perceptron is ¿ready? again

--- blob-detect/src/main/scala/imgproc/ocr/perceptron/Perceptron.scala
+++ blob-detect/src/main/scala/imgproc/ocr/perceptron/Perceptron.scala
@@ -1,5 +1,8 @@
 package imgproc.ocr.perceptron
 
+import java.util
+
+import imgproc.ocr.OneLetterOCR.{LetterProb, LetterResult}
 import imgproc.ocr.Pattern
 import org.opencv.core.{CvType, Mat}
 import org.opencv.ml.CvANN_MLP
@@ -37,24 +40,29 @@ object Perceptron {
       pattern.get(r,c,buffer)
       ret(Pattern.patternSize*c+r) = buffer(0)
     }
+    ret
   }
 
 
   def labelOfLetter( le: Char ) : Int = le - 'A'
   def letterOfLabel( la: Int ) : Char = ('A' + la).toChar
 
-  def train( data: Map[Char,Seq[Mat]], alpha: Double,  beta: Double, activateFunc : Int = CvANN_MLP.SIGMOID_SYM ) = {
-
+  def train( data: Map[Char,Seq[Mat]], alpha: Double,  beta: Double, activateFunc : Int = CvANN_MLP.SIGMOID_SYM ) : CvANN_MLP = {
 
     def trainDataset( data: Map[Char,Seq[Mat]] ) : (Mat,Mat,Mat) = {
       val letters = data.keys.toList
 
+      assert( letters.toSet == (0 until letters.size).map( (_ + 'A').toChar ).toSet )
+
       val labels = {
-        val labelsArray : Array[Byte] = letters.flatMap( l => Iterator.continually(l.toByte).take(data(l).size) ).toArray
+        val labelsArray : Array[Int] = letters.flatMap( l => Iterator.continually(labelOfLetter(l)).take(data(l).size) ).toArray
         val n = labelsArray.size
-        val ret = new Mat(n,1,CvType.CV_8SC1)
+        val ret = new Mat(n,letters.size,CvType.CV_8SC1)
+        val buffer = new Array[Byte](letters.size)
         for( r <- 0 until n ){
-          ret.put(r,0,Array(labelsArray(r)))
+          util.Arrays.fill(buffer,0.toByte)
+          buffer(labelOfLetter(r.toChar)) = 255.toByte
+          ret.put(r,0,buffer)
         }
         ret
       }
@@ -81,6 +89,27 @@ object Perceptron {
     ann.create(layerSizes(data.size), activateFunc, alpha, beta)
 
     val (input,labels,weights) = trainDataset( data )
-    val iterations = ann.train( input, labels, weights )
+    ann.train( input, labels, weights )
+    ann
+  }
+
+  private def fillRowWithArray( m: Mat, row: Int, b: Array[Byte] ){
+    assert(m.cols() == b.size )
+    val buffer = new Array[Byte](1)
+    for( i <- 0 until m.cols() ){
+      buffer(0) = b(i)
+      m.put(row,i,buffer)
+    }
+  }
+
+  def predict( ann: CvANN_MLP, pattern: Mat ) : LetterResult = {
+    val input = patternToInputData(pattern)
+    val mInput = new Mat(1,nodesInInputLayer,CvType.CV_8SC1)
+    fillRowWithArray( mInput, 0, input )
+    val mOutput = new Mat
+    ann.predict(mInput,mOutput)
+
+    val labels = (0 until mOutput.cols()).map( (_ + 'A').toChar )
+    labels.map( l => LetterProb(l, mOutput.get(0,l)(0)/255 ) )
   }
 }
