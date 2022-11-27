13a81a9dafacf33344bbb508f724725107519bd0
alvaro@thinkpad <alvaro@thinkpad>
2022-11-27 17:02:43 +0100

    actualizada plantilla para alumnos con adaptaciones

--- web-server/app/controllers/HomeController.scala
+++ web-server/app/controllers/HomeController.scala
@@ -6,25 +6,36 @@ import play.api.libs.Files
 import play.api.mvc._
 
 import java.nio.file.Paths
+import java.io.File
+
+import common.HKey
+import common.HMap
+
+
+object WebServerInfo {
+
+  object originalImageFileName extends HKey[File]
+
+}
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
-  
+
   def explore() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.explore())
   }
@@ -39,23 +50,26 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
 
   def upload = Action(parse.multipartFormData) { request =>
-    request.body
-      .file("picture")
-      .map { picture =>
+    val files = request.body.files
+
+    if (files.size == 0) {
+      NotAcceptable("Missing file").flashing("error" -> "Missing file")
+    }
+    else{
+      files.foreach { picture =>
         // only get the last part of the filename
         // otherwise someone can send a path like ../../home/foo/bar.txt to write to other files on the system
-        val filename    = Paths.get(picture.filename).getFileName
-        val fileSize    = picture.fileSize
+        val filename = Paths.get(picture.filename).getFileName
+        val fileSize = picture.fileSize
         val contentType = picture.contentType
-        val destFile    = Paths.get(s"/tmp/picture/$filename")
+        val destDir =  Paths.get("/tmp/picture")
+        val destFile = destDir.resolve( filename)
 
-        Paths.get("/tmp/picture").toFile().mkdirs()
+        destDir.toFile().mkdirs()
         picture.ref.copyTo(destFile, replace = true)
-        Ok(s"File uploaded. file:///$destFile . fileSize:$fileSize  contentType:$contentType")
       }
-      .getOrElse {
-        Redirect(routes.HomeController.index()).flashing("error" -> "Missing file")
-      }
+      val fileNames = files.map(_.filename).mkString(",")
+      Ok(s"Files uploaded. $fileNames")
+    }
   }
-  
 }
